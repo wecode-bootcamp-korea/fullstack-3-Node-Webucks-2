7305e0c4deea0202e6398846dec08d79d93bfd2a
Jongmin Choi <quark4904@gmail.com>
2021-12-17 15:11:33 +0900

    Modify: src 폴더 생성 및 signup API 모듈화

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
 const http = require("http");
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
-const { users } = require("./users");
+const { users } = require("./src/users");
+const { signup } = require("./src/signup");
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-app.post("/users/signup", async (req, res) => {
-  try {
-    const { email, username, password, address, phone_number } = req.body;
-
-    console.log("email: ", email, "username: ", username);
-
-    const createdUser = await prisma.$queryRaw`
-        insert into users(email, password, username, address, phone_number) values (${email}, ${password}, ${username}, ${address}, ${phone_number});`;
-
-    return res.status(201).json({ message: "CREATED" });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: err.message });
-  }
-});
+app.post("/users/signup", signup);
 
 app.get("/users", users);
 
---
+++ src/signup.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const signup = async (req, res) => {
+  try {
+    const { email, username, password, address, phone_number } = req.body;
+
+    console.log("email: ", email, "username: ", username);
+
+    const createdUser = await prisma.$queryRaw`
+    insert into users(email, password, username, address, phone_number) values (${email}, ${password},${username},${address},${phone_number});`;
+
+    return res.status(201).json({ message: "CREATED" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { signup };
