9f0b34da7271bd08cc710721f15300016e8a787c
Jongmin Choi <quark4904@gmail.com>
2021-12-24 16:03:19 +0900

    Add: cors 설치

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import http from "http";
 import express from "express";
 import routes from "./routes";
+import cors from "./cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors);
 
 app.use((err, req, res, next) => {
   const { status, message } = err;
